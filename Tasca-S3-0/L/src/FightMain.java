0afa88b2d17acddfd82d9fff89b6428d5654784b
erictc95 <maxerictc95@hotmail.com>
2026-04-21 12:54:21 +0200

    Update Code to get finished.

--- Tasca-S3-0/L/src/FightMain.java
+++ Tasca-S3-0/L/src/FightMain.java
@@ -1,5 +1,14 @@
 public class FightMain {
     public static void main (String[] args) {
+        Warrior warrior = new Warrior("Nakamura");
 
+        Damage wDamage = warrior;
+        Attack wAttack = warrior;
+
+        Attack gAttack = new Ghost("Gengar");
+
+        wDamage.takeDamage(20);
+        wAttack.attack();
+        gAttack.attack();
     }
 }
